430d73c2b9d0b01bcf189152cad783ee29221aed
Bianca Besciu <bianca.besciu@gmail.com>
2017-10-30 14:06:52 +0100

    create static components

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Playlist.css';
+import TrackList from '../TrackList/TrackList';
+
+class Playlist extends Reac.Component {
+    render() {
+        return(
+            <div className="Playlist">
+                <input value="New Playlist"/>
+                <!-- Add a TrackList component -->
+                <a className="Playlist-save">SAVE TO SPOTIFY</a>
+            </div>
+        )
+    }
+}
